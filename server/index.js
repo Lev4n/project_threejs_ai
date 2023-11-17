4bee6c512db3313ebaf0b9f797a9f28269e7f1e6
Levan Lomidze <levan.lomo@gmail.com>
2023-11-17 19:47:42 +0400

    Fixed openai image generation issue, function was outdated

--- server/index.js
+++ server/index.js
@@ -8,11 +8,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-}));
+app.use(cors());
 
-app.use(express.json({ limit: "50mb" }))
+app.use(express.json({ limit: "100mb" }))
 
 app.use("/api/v1/dalle", dalleRoutes);
 
